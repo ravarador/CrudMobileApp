a480448cb3fd6fc9f906382b69b68d90ee122a72
Ravni Arador <ravarador@gmail.com>
2021-08-03 20:52:57 -0400

    Learned content page navigation and using commands

--- CrudMobileApp/CrudMobileApp/ViewModels/AddProductViewModel.cs
+++ CrudMobileApp/CrudMobileApp/ViewModels/AddProductViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CrudMobileApp.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace CrudMobileApp.ViewModels
 {
@@ -13,9 +16,32 @@ namespace CrudMobileApp.ViewModels
         int id = 23112;
         string productName = "Ravni Arador";
 
+        public AddProductViewModel()
+        {
+            SaveProductCommand = new Command(SaveProduct);
+
+            OpenProductBrowserCommand = new Command(async () => await OpenProductBrowser());
+        }
         void OnPropertyChanged([CallerMemberName]string name = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public Command OpenProductBrowserCommand { get; }
+        public Command SaveProductCommand { get; }
+
+        async Task OpenProductBrowser()
+        {
+            var productBrowserViewModel = new ProductBrowserViewModel();
+            var productBrowser = new ProductBrowser();
+            productBrowser.BindingContext = productBrowserViewModel;
+
+            await Application.Current.MainPage.Navigation.PushAsync(productBrowser);
+        }
+
+        void SaveProduct()
+        {
+
         }
 
         public string ProductName
---
+++ CrudMobileApp/CrudMobileApp/Views/ProductBrowser.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrudMobileApp.Views
+{
+    public partial class ProductBrowser : ContentPage
+    {
+        public ProductBrowser()
+        {
+            InitializeComponent();
+
+            
+        }
+    }
+}
